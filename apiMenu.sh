d67da534902d2e43de0efd6dad42ad488ebdbfcc
J.F. Roque <123225396+Cyber-ssh@users.noreply.github.com>
2024-03-07 06:24:22 -0400

    Add files via upload

--- apiMenu.sh
+++ apiMenu.sh
@@ -94,9 +94,9 @@ while true; do
     fi
 
     echo -e "\nSelecione uma opção:"
-    echo -e "\033[1;31m[\033[1;36m1\033[1;31m] \033[1;37m• \033[1;33m 1 - $acao api"
-    echo -e "\033[1;31m[\033[1;36m1\033[1;31m] \033[1;37m• \033[1;33m 2 - Sobre"
-    echo -e "\033[1;31m[\033[1;36m1\033[1;31m] \033[1;37m• \033[1;33m 0 - Sair do menu"
+    echo -e "\033[1;31m[\033[1;36m1\033[1;31m] \033[1;37m• \033[1;33m 01 - $acao api"
+    echo -e "\033[1;31m[\033[1;36m1\033[1;31m] \033[1;37m• \033[1;33m 02 - Sobre"
+    echo -e "\033[1;31m[\033[1;36m1\033[1;31m] \033[1;37m• \033[1;33m 00 - Sair do menu"
 
     read -p "Digite a opção: " option
 
